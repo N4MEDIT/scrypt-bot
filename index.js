7dee741cbb757c98265e8540c3141e88d8e9b206
N4MEDIT <stiilexyt@gmail.com>
2019-05-08 04:58:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const cfg require('./index.json'); // a garder en version desktop
+const token = process.env.token // a garder en version heroku
+const prefix = (".");
+
+bot.on('ready', function () {
+    console.log("Je suis prêt utilisez-moi quand vous voulez.")
+    bot.user.setActivity('rien').catch(console.error)
+});
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('Bienvenue sur le serveur de ScryptFamily' + member.displayName)
+        console.log(`${member.displayName} à rejoind le serveur.`)
+    }).catch(console.error)
+});
+
+const ban = require('./kick et ban/ban');
+require('./embed/RichEmbed');
+
+bot.on('message', function (message){
+    if (ban.match(message)){
+        return ban.action(message)
+    }
+});
+
+
+bot.on('message', msg => {
+    if (msg.content === "bonjour"){
+        msg.reply("Heureux de te revoir parmis nous.")
+    }
+    if (msg.content.match(/salut/i)) {
+            msg.reply('Je suis d\'accord avec toi.')
+    }
+    if (msg.content === prefix + "youtube"){
+        msg.channel.send("https://www.youtube.com/channel/UCp3k3CACniNqORiJ-qkyPRQ")
+        console.log("Une personne a demandé ta chaine youtube.")
+    }
+
+});
+
+bot.login(cfg.token); //a garder en version desktop
+bot.login(token); //a garder en version heroku
